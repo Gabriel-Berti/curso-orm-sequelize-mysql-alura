c8c5314afd593c939cc9b743b4beaa03e1d619be
Gabriel Berti <gabriel.berti@linainfratech.com.br>
2022-12-07 13:33:53 -0300

    CRUD com Sequelize - Nesta aula, aprendemos a:
    Utilizar outros métodos do Sequelize para as operações de CRUD
    Enviar dados através de parâmetros de requisição HTTP
    Enviar dados através do corpo da requisição HTTP
    Criar rotas para cada operação
    Associar as rotas a cada método do controlador Pessoas

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -10,6 +10,62 @@ class PessoaController {
       res.status(500).json(error.message);
     }
   }
+  static async pegaUmaPessoa(req, res) {
+    try {
+      const { id } = req.params;
+      const pessoa = await database.Pessoas.findOne({
+        where: { id: Number(id) },
+      });
+      if (pessoa === null) {
+        return res.status(404).send("Pessoa não encontrada no banco de dados.");
+      } else {
+        return res.status(200).json(pessoa);
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error.message);
+    }
+  }
+  static async criaPessoa(req, res) {
+    try {
+      const dadosNovaPessoa = req.body;
+      const novaPessoa = await database.Pessoas.create(dadosNovaPessoa);
+      return res.status(201).json(novaPessoa);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error.message);
+    }
+  }
+  static async atualizaPessoa(req, res) {
+    const { id } = req.params;
+    const dadosAtualizados = req.body;
+    try {
+      await database.Pessoas.update(dadosAtualizados, {
+        where: { id: Number(id) },
+      });
+      const pessoaAtualizada = await database.Pessoas.findOne({
+        where: { id: Number(id) },
+      });
+      return res.status(201).json(pessoaAtualizada);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error.message);
+    }
+  }
+  static async deletaPessoa(req, res) {
+    const { id } = req.params;
+    try {
+      await database.Pessoas.destroy({
+        where: { id: Number(id) },
+      });
+      return res.status(201).send({
+        mensagem: `A pessoa com o id ${id} foi deletada.`,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error.message);
+    }
+  }
 }
 
 module.exports = PessoaController;
